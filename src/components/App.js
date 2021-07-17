b7bd2c40fb7472720af1f3b8d3565fb481924cfd
daleinen7 <daleinen@gmail.com>
2021-07-16 20:34:01 -0500

    router and register component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
+import Register from '../pages/Register';
 
 function App() {
-  return <Home />;
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/register">
+        <Register />
+      </Route>
+    </Switch>
+  )
 }
 
 export default App;
