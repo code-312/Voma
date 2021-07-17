cf248771c4d9f2db7a35129b55ca28905989d5dd
daleinen7 <daleinen@gmail.com>
2021-07-17 12:09:53 -0500

    undo global style setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,11 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom';
-import { createGlobalStyle } from 'styled-components'
 import Home from '../pages/Home';
 import Register from '../pages/Register';
-
-const GlobalStyle = createGlobalStyle`
-  background: #456123;
-`
 
 function App() {
   return (
     <>
-      <GlobalStyle />
       <Switch>
         <Route exact path="/">
           <Home />
