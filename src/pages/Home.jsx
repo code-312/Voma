1f09d1440c28548fa036aee1e7c533635d3b622c
Kim Rasmussen <68238123+kimrass14@users.noreply.github.com>
2021-07-21 22:45:11 -0500

    styled components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
 import styled from 'styled-components';
 
-const StyledPage = styled.page`
-        color: red;
-    `;
+const StyledSection = styled.section`
+    width: 50%;
+    margin: 0 auto;
+    padding-top: 100px;
+    font-family: 'IBM Plex Sans', sans-serif;
+`;
+
+const StyledH1 = styled.h1`
+    font-size: 48px;
+    font-weight: 600;
+`;
+
+const StyledParagraph = styled.p`
+    font-size: 18px;
+`;
+
+const StyledImg = styled.img`
+    width: 50%;
+`;
+
+const StyledA = styled.a`
+    color: blue;
+    text-decoration: underline;
+`;
 
 export default function Home() {
     return (
-        <StyledPage>
-            <div>
-                <h1>Sign in with Slack</h1>
-                <h2>We utilize Slack throughout our onboarding process. If you haven&#39;t already, please join our Slack and come back here to sign in.</h2>
-                <img src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" alt="slack button" />
-            </div>
-        </StyledPage>
+        <StyledSection>
+                <StyledH1>Sign in with Slack</StyledH1>
+                <StyledParagraph>We utilize Slack throughout our onboarding process. If you haven&#39;t already, please join our <StyledA href="https://code-for-chicago-slack-invite.herokuapp.com/">Slack</StyledA> and come back here to sign in.</StyledParagraph>
+                <StyledImg src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" alt="slack button" />
+        </StyledSection>
     )
 }
