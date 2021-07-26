0f6dc7d0543f69d66baebbe1dbadda061558e73a
daleinen7 <daleinen@gmail.com>
2021-07-26 12:57:07 -0500

    add no onboarding page

---
+++ src/pages/NoOnboarding.jsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  width: 50vw;
+  margin: 0 auto;
+
+  h1 {
+    padding: 3rem 0 1rem 0;
+    font-size: 2rem;
+    font-weight: 700;
+  }
+
+  p {
+    font-size: 1rem;
+  }
+`;
+
+export default function NoOnboarding() {
+  return (
+    <StyledDiv>
+      <h1>We appreciate your enthusiasm!</h1>
+      <p>
+        At this time new members are required to attend an Onboarding Night. If you haven&apos;t
+        attened Onboarding Night yet then register and attened one through our Meetup Page: &nbsp;
+        <a href="https://www.meetup.com/code-for-chicago">
+          https://www.meetup.com/code-for-chicago
+        </a>
+      </p>
+      <p>We&apos;d love to see you then!</p>
+
+      <Link to="/">
+        <button type="button">Start Over</button>
+      </Link>
+    </StyledDiv>
+  );
+}
