8aa25bd3ff4e7b8d81cf9008d09453ec031f1dd4
kylefarmer85 <kylefarmer85@gmail.com>
2021-08-02 20:48:31 -0500

    change code of conduct guidelines to an ordered list

--- src/components/CodeOfConduct.jsx
+++ src/components/CodeOfConduct.jsx
@@ -14,34 +14,31 @@ export default function CodeOfConduct({ setRegisterStep }) {
       <br />
       <p>Code for Chicago&apos;s Code of Conduct</p>
       <p>
-        Adapted from the{' '}
+        Adapted from the
         <a href="https://github.com/codeforamerica/codeofconduct">CfA Code of Conduct</a>. We are an
         official brigade. <br />
         The Code for Chicago community expects that Code for Chicago events, and digital forums:
       </p>
-      <p>
-        1. Are a safe and respectful environment for all participants. <br />
-        2. Are a place where people are free to fully express their identities. <br />
-        3. Presume the value of others. Everyone&apos;s ideas, skills, and contributions have value.{' '}
-        <br />
-        4. Don’t assume everyone has the same context, and encourage questions. <br />
-        5. Find a way for people to be productive with their skills (technical and not) and energy.
-        Use language such as “yes/and”, not “no/but.” <br />
-        6. Encourage members and participants to listen as much as they speak. <br />
-        7. Strive to build tools that are open and free technology for public use. Activities that
-        aim to foster public use, not private gain, are prioritized. <br />
-        8. Prioritize access for and input from those who are traditionally excluded from the civic
-        process. <br />
-        9. Work to ensure that the community is well-represented in the planning, design, and
+      <ol>
+        <li>Are a safe and respectful environment for all participants.</li>
+        <li>Are a place where people are free to fully express their identities.</li>
+        <li>Presume the value of others. Everyone&apos;s ideas, skills, and contributions have value.</li>
+        <li>Don’t assume everyone has the same context, and encourage questions.</li>
+        <li>Find a way for people to be productive with their skills (technical and not) and energy. Use language such as “yes/and”, not “no/but.”</li>
+        <li>Encourage members and participants to listen as much as they speak.</li>
+        <li>Strive to build tools that are open and free technology for public use. Activities that aim to foster public use, not private gain, are prioritized.</li>
+        <li>Prioritize access for and input from those who are traditionally excluded from the civic process.</li>
+        <li>Work to ensure that the community is well-represented in the planning, design, and
         implementation of civic tech. This includes encouraging participation from women,
-        minorities, and traditionally marginalized groups. <br />
-        10. Actively involve community groups and those with subject matter expertise in the
-        decision-making process. <br />
-        11. Ensure that the relationships and conversations between community members, the local
-        government staff and community partners remain respectful, participatory, and productive.{' '}
-        <br />
-        12. Provide an environment where people are free from discrimination or harassment. <br />
-      </p>
+        minorities, and traditionally marginalized groups. </li>
+        <li>Actively involve community groups and those with subject matter expertise in the
+        decision-making process.</li>
+        <li>Ensure that the relationships and conversations between community members, the local
+        government staff and community partners remain respectful, participatory, and productive.
+        </li>
+        <li>Provide an environment where people are free from discrimination or harassment.</li>
+      </ol>
+      <br />
       <p>
         Code for Chicago reserves the right to ask anyone in violation of these policies not to
         participate in Code for Chicago network activities, events, and digital forums.
