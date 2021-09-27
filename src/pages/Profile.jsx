b585b04a72a4f07592fee3c70df4c4025f5cc00f
mw18 <67554339+mw18@users.noreply.github.com>
2021-09-27 16:43:22 -0500

    format with prettier and update styling

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,44 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import StyledFieldset from '../components/StyledFieldset'
+import StyledFieldset from '../components/StyledFieldset';
 
 export default function Profile() {
-  
   const profile = {
     firstName: 'Joseph',
     lastName: 'Tajaran',
     email: 'joseph@tajaran.com',
     pronouns: 'he/him',
     skill: 'UX Design',
+  };
 
-  }
-
-  return(
+  return (
     <StyledFieldset>
-    <form>
-      <legend>{profile.firstName} {profile.lastName}</legend>
-      <p>  <Link to="/"> Home</Link>  /  {profile.firstName} {profile.lastName} <Link to="/signOut" style={{float: 'right'}}> Sign Out</Link></p>
+      <form>
+        <legend>
+          {profile.firstName} {profile.lastName}
+        </legend>
+        <p>
+          {' '}
+          <Link to="/"> Home</Link> / {profile.firstName} {profile.lastName}{' '}
+          <Link to="/signOut" style={{ float: 'right' }}>
+            {' '}
+            Sign-Out
+          </Link>
+        </p>
 
-       <legend>Basic Info</legend>
-       
-        <p>Email<br/>
-        {profile.email}</p>
+        <legend>Basic Info</legend>
 
-        <p>Full Name<br/>
-        {profile.firstName} {profile.lastName}</p>
+        <p>
+          Email
+          <h2> {profile.email} </h2>
+        </p>
 
-        <p>Pronouns<br/>
-        {profile.pronouns}</p>
-  
-       <legend>Skills</legend>
+        <p>
+          Full Name
+          <h2>
+            {profile.firstName} {profile.lastName}
+          </h2>
+        </p>
 
-        <p>Primiary Skills<br/>
-        {profile.skill}</p>
+        <p>
+          Pronouns
+          <h2>{profile.pronouns}</h2>
+        </p>
 
+        <legend>Skills</legend>
+
+        <p>
+          Primiary Skills
+          <h2>{profile.skill}</h2>
+        </p>
       </form>
-
-      </StyledFieldset>
-
-  )
-
+    </StyledFieldset>
+  );
 }
