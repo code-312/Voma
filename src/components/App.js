a3d5fdc9f8020552dc6e6ce347caf3625373c948
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-10-08 09:43:32 -0500

    page finished

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import { Switch, Route } from 'react-router-dom';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Home from '../pages/Home';
 import Register from '../pages/Register';
+import Dashboard from '../pages/Dashboard';
+
 
 function App() {
   return (
@@ -21,6 +23,9 @@ function App() {
         </Route>
         <Route path="/register">
           <Register />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
         </Route>
       </Switch>
     </HelmetProvider>
