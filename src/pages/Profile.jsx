1ae4aa1da650d54bbae1e66da3045db4c1fbad2e
mw18 <67554339+mw18@users.noreply.github.com>
2021-10-11 16:24:35 -0500

    remove fieldset, update tags, remove blank strings, spellcheck, implement updated styling

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import StyledFieldset from '../components/StyledFieldset';
+import styled from 'styled-components';
+
+const StyledObject = styled.section`
+  width: 50vw;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    padding: 3rem 0 1rem 0;
+    font-size: 3rem;
+    font-weight: 700;
+  }
+
+  h2 {
+    padding: 2rem 0 1rem 0;
+    font-size: 2rem;
+    font-weight: 700;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+  }
+
+  p {
+    font-size: 1rem;
+  }
+`;
 
 export default function Profile() {
   const profile = {
@@ -12,46 +39,43 @@ export default function Profile() {
   };
 
   return (
-    <StyledFieldset>
-      <form>
-        <legend>
+    <StyledObject>
+      <h1>
+        {profile.firstName} {profile.lastName}
+      </h1>
+
+      <p>
+        <Link to="/"> Home</Link> / {profile.firstName} {profile.lastName}{' '}
+        <Link to="/signOut" style={{ float: 'right' }}>
+          Sign-Out
+        </Link>
+      </p>
+
+      <h2>Basic Info</h2>
+
+      <p>
+        Email
+        <h3> {profile.email} </h3>
+      </p>
+
+      <p>
+        Full Name
+        <h3>
           {profile.firstName} {profile.lastName}
-        </legend>
-        <p>
-          {' '}
-          <Link to="/"> Home</Link> / {profile.firstName} {profile.lastName}{' '}
-          <Link to="/signOut" style={{ float: 'right' }}>
-            {' '}
-            Sign-Out
-          </Link>
-        </p>
+        </h3>
+      </p>
 
-        <legend>Basic Info</legend>
+      <p>
+        Pronouns
+        <h3>{profile.pronouns}</h3>
+      </p>
 
-        <p>
-          Email
-          <h2> {profile.email} </h2>
-        </p>
+      <h2>Skills</h2>
 
-        <p>
-          Full Name
-          <h2>
-            {profile.firstName} {profile.lastName}
-          </h2>
-        </p>
-
-        <p>
-          Pronouns
-          <h2>{profile.pronouns}</h2>
-        </p>
-
-        <legend>Skills</legend>
-
-        <p>
-          Primiary Skills
-          <h2>{profile.skill}</h2>
-        </p>
-      </form>
-    </StyledFieldset>
+      <p>
+        Primary Skills
+        <h3>{profile.skill}</h3>
+      </p>
+    </StyledObject>
   );
 }
