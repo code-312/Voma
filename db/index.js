866d8794bc59dc350930508f4b609c7bcb242d97
raindogg <wabray17@gmail.com>
2021-11-07 09:50:10 -0600

    Add sequlize, connect to database, create models, create env variables

--- db/index.js
+++ db/index.js
@@ -1,22 +1,47 @@
-const { Pool, Client } = require('pg')
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'mydb',
-  password: 'password',
-  port: 5432,
-})
+const { Sequelize } = require('sequelize');
 
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-module.exports = {
-  getUsers: async () => {
-    try {
-      const result = await pool.query('SELECT * FROM users');
-      console.log(result);
-      return result.rows;
+const seq = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
-    catch (err) {
-      throw new Error(err);
-    }
+});
+
+const modelDefiners = [
+	require('./models/volunteer.model'),
+	require('./models/project.model'),
+	require('./models/skill.model'),
+];
+
+for (const modelDefiner of modelDefiners) {
+	modelDefiner(seq);
+}
+
+const test = async () => {
+  console.log('trying to connect');
+  try {
+    await seq.authenticate();
+    console.log('Connection success!');
+  } catch (err) {
+    console.log(`Err :( ${err}`);
   }
-};
+}
+
+const sync = async () => {
+  console.log('Syncing the database');
+  try {
+    await seq.sync({ alter: true });
+    console.log('Database synced!');
+  } catch (err) {
+    console.log(`Err :( ${err})`);
+  }
+}
+
+sync();
+  
+module.exports = seq;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require('dotenv').config({ path: `./.env.${process.env.NODE_ENV}`});
 const express = require('express');
 const path = require('path');
-const { getUsers } = require('./db/index');
+const { models } = require('./db');
 
 const app = express();
 
@@ -9,10 +10,32 @@ app.use(express.static(path.join(__dirname, '/client/build')));
 
 // Put all API endpoints under '/api'
 app.get('/api/test', async (req, res) => {
-   const result = await getUsers();
+  //  const result = await getUsers();
+    const result = test();
     res.send(JSON.stringify(result));
 });
 
+app.get('/api/addUser', async (req, res) => {
+  try {
+    await models.volunteer.create({
+      name: "Will Bray",
+      email: "wabray17@gmail.com"
+    });
+    res.send("Success!");
+  } catch (err) {
+    res.send(`Uh oh.... ${err}`);
+  }
+});
+
+app.get('/api/getUsers', async (req, res) => {
+    try {
+      const volunteers = await models.volunteer.findAll();
+      res.send(JSON.stringify(volunteers));
+    } catch (err) {
+      res.send("Err!");
+    }
+});
+  
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
