c1d62ec53f03eb364f50964580fb0b783d9e9967
Brian Wagner <briwagner@outlook.com>
2021-12-17 21:41:24 -0600

    skills: add controller and routes for basic Skills model

---
+++ db/controllers/skills.controller.js
@@ -0,0 +1,72 @@
+const { seq } = require('../index.js');
+const { models } = require('../index');
+
+exports.getSkills = async (req, res) => {
+  var dbError
+  const skills = await models.skill.findAll()
+      .catch(err => dbError = err);
+
+  if (dbError) { return res.status(400).json({"error": dbError}); }
+
+  res.json(skills)
+};
+
+exports.getSkill = async (req, res) => {
+    const skill = await models.skill.findByPk(req.params.id);
+    if (skill === null) {
+        return res.sendStatus(404);
+    }
+    res.json(skill);
+};
+
+exports.addSkill = async (req, res) => {
+    const {
+        name,
+        description
+    } = req.body;
+
+    var dbError;
+    const result = await models.skill.create({
+        name,
+        description
+    }).catch(err => dbError = err);
+
+    if (dbError) { return res.status(400).json({"error": err}); }
+
+    res.json(result);
+};
+
+exports.editSkill = async (req, res) => {
+    const skill = await models.skill.findByPk(req.params.id);
+    if (skill === null) {
+        return res.sendStatus(404);
+    }
+
+    const {
+        name,
+        description
+    } = req.body;
+
+    var dbError;
+    skill.update({name: name, description: description})
+      .catch(err => dbError = err);
+
+    if (dbError) { return res.status(400).json({"error": dbError }); }
+
+    res.json(skill);
+};
+
+exports.removeSkill = async (req, res) => {
+    const skill = await models.skill.findByPk(req.params.id);
+    if (skill === null) {
+        return res.sendStatus(404);
+    }
+
+    try {
+        // Delete always returns [].
+        const result = await skill.destroy()
+        res.json({"status": `skill ${req.params.id} removed`});
+    } catch (err) {
+        res.status(400).json({"error": err});
+    }
+};
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 require('dotenv').config({ path: `./.env.${process.env.NODE_ENV}`});
 const express = require('express');
 const path = require('path');
-const { 
-  getVolunteers, 
-  getVolunteer, 
-  addVolunteer, 
+const {
+  getVolunteers,
+  getVolunteer,
+  addVolunteer,
   editVolunteer,
   removeVolunteer
 } = require('./db/controllers/volunteers');
+
+const skillsController = require('./db/controllers/skills.controller');
 
 const app = express();
 app.use(express.json());
@@ -31,7 +33,14 @@ app.post('/api/volunteer/:id', editVolunteer);
 
 /* Remove a volunteer */
 app.delete('/api/volunteer/:id', removeVolunteer);
-  
+
+/* Skills routes */
+app.get('/api/skills', skillsController.getSkills);
+app.get('/api/skills/:id', skillsController.getSkill);
+app.post('/api/skills', skillsController.addSkill)
+app.put('/api/skills/:id', skillsController.editSkill)
+app.delete('/api/skills/:id', skillsController.removeSkill)
+
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
