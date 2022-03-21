436f3ac6af99ec20a84a5f3d33e191a64b739e2f
raindogg <wabray17@gmail.com>
2022-03-21 13:29:00 -0500

    Save volunteers information to DB when they finish going through register pages

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import BasicInfo from '../components/BasicInfo';
@@ -7,15 +6,44 @@ import CodeOfConduct from '../components/CodeOfConduct';
 import ThankYou from '../components/ThankYou';
 import NoOnboarding from './NoOnboarding';
 import RegisterLanding from '../components/RegisterLanding';
+import { User } from '../models/user.model';
 
 export default function Register({ userDetails = null }) {
   const [formData, setFormData] = useState({
+    emailAddress: '',
     slackId: '',
-    firstName: '',
-    lastName: '',
+    fullName: '',
     pronouns: '',
     skill: '',
   });
+
+  const saveUser = () => {
+    // e.preventDefault()
+
+    const user = new User(formData.fullName, formData.emailAddress, formData.slackId, formData.pronouns);
+    fetch('/api/volunteer', {
+      method: 'POST',
+      body: JSON.stringify({
+        "name": user.name,
+        "email": user.email,
+        "pronouns": user.pronouns,
+        "slackUserId": user.slackUserId,
+        "skills": formData.skill,
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+        .then((res) => {
+          if (res.status === 404) {
+            throw new Error();
+          } else {
+            // Redirect??
+            return res.json();
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+  }
 
   useEffect(() => {
     if (userDetails) {
@@ -58,7 +86,7 @@ export default function Register({ userDetails = null }) {
 
         />
       )}
-      {registerStep === 4 && <CodeOfConduct setRegisterStep={setRegisterStep} />}
+      {registerStep === 4 && <CodeOfConduct setRegisterStep={setRegisterStep} saveUser={saveUser} />}
       {registerStep === 5 && <ThankYou setRegisterStep={setRegisterStep} />}
     </form>
   );
--- db/sync.js
+++ db/sync.js
@@ -13,6 +13,7 @@ const modelDefiners = [
 	require('./models/volunteer.model'),
 	require('./models/project.model'),
 	require('./models/skill.model'),
+  require('./models/volunteerSkills.model')
 ];
 
 for (const modelDefiner of modelDefiners) {
