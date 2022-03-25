ba2e8a5da38e48b560da3308463994e712acf54f
adam <innes@50000feet.com>
2022-03-25 18:54:15 -0500

    Removed the hard coded color and stubbed out the Sign In button in the top navigation.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Container } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 
 export default function Header() {
     return (
-        <AppBar position="static" style={{ backgroundColor: '#6200EE' }}>
-            <Toolbar>
-                <Typography variant="h6" noWrap component="div">Code for Chicago</Typography>
-            </Toolbar>
-        </AppBar>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>Code for Chicago</Typography>
+                    <Button color="inherit">Sign In</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
     )
 }
