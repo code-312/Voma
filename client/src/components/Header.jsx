2a57c33ec4ffb8b740d506029c4f7786ddb813bb
Adam <innesian@github.com>
2022-03-31 18:17:23 -0500

    Removing the hamburger menu (Figma notes have it out of scope). Using "Code for Chicago" as the main title, some Figma screens have a different title. Cleaning up imports.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button, Box, IconButton } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 
 export default function Header() {
     return (
         <Box mb="32px" sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
+                    {/* Figma notes has the hamburger menu out of scope */}
+                    {/* <IconButton
                         size="large" 
                         edge="start" 
                         sx={{ mr: 2 }}
                         color="inherit" 
                         aria-label="menu">
                         <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>Voma</Typography>
+                    </IconButton> */}
+                    <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>Code for Chicago</Typography>
                     <Button color="inherit">Sign In</Button>
                 </Toolbar>
             </AppBar>
