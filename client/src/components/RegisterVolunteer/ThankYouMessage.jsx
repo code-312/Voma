e47d99e9a4f0119dd16865434e99b83c8cd2890f
Adam <innesian@github.com>
2022-04-01 07:56:13 -0500

    Add the Grid to the Thank You page, tweaked the styling.

--- client/src/components/RegisterVolunteer/ThankYouMessage.jsx
+++ client/src/components/RegisterVolunteer/ThankYouMessage.jsx
@@ -1,14 +1,15 @@
-import Typography from '@mui/material/Typography';
-import MUIFieldsetStyles from '../MUIStyledFieldSet';
+import { Typography, Grid } from '@mui/material';
 
 export default function ThankYouMessage() {
-  return (
-    <MUIFieldsetStyles>
-      <Typography variant="h4" component="h1"> Thank You!</Typography>
-      <Typography>
-        Thanks for taking the time to complete registration. Our onboarding team will reach out to
-        you soon through Slack. In the meantime, please introduce yourself in the #intros channel.
-      </Typography>
-    </MUIFieldsetStyles>
-  );
+  return (<>
+    <Grid container justifyContent="center">
+      <Grid item sm={6} xs={12}>
+        <Typography variant="h4" component="h1" mb='16px'> Thank You!</Typography>
+        <Typography>
+          Thanks for taking the time to complete registration. Our onboarding team will reach out to
+          you soon through Slack. In the meantime, please introduce yourself in the #intros channel.
+        </Typography>
+      </Grid>
+    </Grid>
+  </>);
 }
