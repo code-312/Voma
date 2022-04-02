df611edd2a0e9798d7553cab12f66e61b3164720
Adam <innesian@github.com>
2022-04-02 18:23:37 -0500

    Hooking up the AuthProvider to the sign in/out button.

--- client/src/components/LoginToggleButton.jsx
+++ client/src/components/LoginToggleButton.jsx
@@ -1,10 +1,24 @@
+import { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
 import { Button } from '@mui/material';
 
+import { AuthContext } from '../lib/AuthProvider';
+
 export default function LoginToggleButton() {
-    const loggedIn = true; // Update when user is added.
+    const UserAuth = useContext(AuthContext);
 
     return (<>
-        {loggedIn && <Button color="inherit">Sign In</Button>}
-        {!loggedIn && <Button color="inherit">Sign Out</Button>}
+        {!UserAuth.isAuthenticated() && 
+            <Button color="inherit"
+                onClick={() => (<Redirect to="/login" />)}>
+                Sign In
+            </Button>
+        }
+        {UserAuth.isAuthenticated() && 
+            <Button color="inherit"
+                onClick={() => UserAuth.logout()}>
+                Sign Out
+            </Button>
+        }
     </>)
 }
