fd248e91502574941c8ba66c881c742e16d4a9a9
Adam <innesian@github.com>
2022-04-08 19:17:40 -0500

    Adding better messaging to the testing steps.

--- client/tests/forms/VolunteerRegistrationForm.js
+++ client/tests/forms/VolunteerRegistrationForm.js
@@ -9,31 +9,29 @@ module.exports = {
         
         // assert that back and front end running (ports 3000, 5000)
         let response = request('GET', 'http://localhost:3000');
-        console.log('Checking if client is running...');
-        assert.equal(response.statusCode, 200);
+        assert.equal(response.statusCode, 200, 'Checking if client is running on port 3000');
 
         response = request('GET', 'http://localhost:5000');
-        console.log('Checking if server is running...');
-        assert.equal(response.statusCode, 200);
+        assert.equal(response.statusCode, 200, 'Checking if server is running on port 5000');
 
         browser
             .url('http://localhost:3000/')
             .waitForElementVisible('#root', 5000)
-            .assert.elementPresent(SlackButtonDOMQuery)
+            .assert.elementPresent(SlackButtonDOMQuery, 'Registration (Step 1)')
             .click(SlackButtonDOMQuery)
             .waitForElementVisible(ModalDOMQuery)
             .setValue('input#email', 'voma.code.for.chicago@gmail.com')
             .click(ModalDOMQuery + ' button:nth-of-type(2)')
-            .assert.elementPresent('input#pronouns')
+            .assert.elementPresent('input#pronouns', 'Registration (Step 2)')
             .setValue('input#pronouns', 'test/test/test')
             .click('#root form button')
-            .assert.elementPresent('input[type="radio"]')
+            .assert.elementPresent('input[type="radio"]', 'Registration (Step 3)')
             .click('input[type="radio"]')
             .click('form button:nth-of-type(2)')
-            .assert.elementPresent(COCButtonQuery)
+            .assert.elementPresent(COCButtonQuery, 'Registration (Step 4)')
             .click(COCButtonQuery)
             .click('form button:nth-of-type(2)')
-            .assert.containsText('h1', 'Thank You!')
+            .assert.containsText('h1', 'Thank You!', 'Registration (Step 5)')
             .end();
 
     }
