4bb0df083745fbd5ddff9b03f71243442799f4f5
Adam <innesian@github.com>
2022-04-09 15:19:53 -0500

    Updating the Lando database sync to add an admin user for testing.

--- .lando.database.sh
+++ .lando.database.sh
@@ -8,4 +8,10 @@ if ! grep -q DB_PORT .env.development.local ; then echo "DB_PORT='6543'" >> .env
 
 if [ ! -f db/.env.development.local ] ; then cp .env.local db/.env.development.local; fi
 
+#cd db && ../node_modules/sequelize-cli/lib/sequelize db:migrate
+
 npm run sync;
+echo "insert into admins (name, email, password, created_at, updated_at) values ('Voma Testing', 'voma.code.for.chicago@gmail.com', '\$2b\$10\$86Ihm1X.d3p6Cz6HncWpM./MrqGIB8yXj0RHj4GQPwhZiTqunVhSi', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);" > add_admin.psql;
+lando ssh -s postgresService -c "psql -U postgres -d voma -f ./add_admin.psql";
+rm add_admin.psql;
+
