f2470cd7d1a4b6903c12f63c73f5301c81c41a9d
Adam <innesian@github.com>
2022-04-10 08:14:28 -0500

    Condensing the database script a bit.

--- .lando.database.sh
+++ .lando.database.sh
@@ -25,33 +25,19 @@ skills=("Content Strategy" "Data Analytics" "Front-End or Back-End Development" 
 echo "insert into admins (name, email, password, created_at, updated_at) values ('Voma Testing', 'voma.code.for.chicago@gmail.com', '\$2b\$10\$86Ihm1X.d3p6Cz6HncWpM./MrqGIB8yXj0RHj4GQPwhZiTqunVhSi', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);" > seed.psql;
 
 # Add test Projects.
-for i in "${projects[@]}"
-do
-    echo "insert into projects (name, description, created_at, updated_at) values ('$i', '$i', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);" >> seed.psql;
-done
+for i in "${projects[@]}" ; do echo "insert into projects (name, description, created_at, updated_at) values ('$i', '$i', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);" >> seed.psql; done
 
 # Add test Skills 
-for i in "${skills[@]}"
-do
-    echo "insert into skills (name, description, created_at, updated_at) values ('$i', '$i', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);" >> seed.psql;
-done
+for i in "${skills[@]}" ; do echo "insert into skills (name, description, created_at, updated_at) values ('$i', '$i', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);" >> seed.psql; done
 
 # Add test Volunteers assigned to random projects.
-for i in "${volunteers[@]}"
-do
-    echo "insert into volunteers (name, email, slack_user_id, pronouns, project_id, created_at, updated_at) values ('$i', 'fake.volunteer.$(((RANDOM%99999)+1))@test.null', 'U03AU1WDP5Z', 'lorum/ipsum', '$(((RANDOM%4)+1))', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);" >> seed.psql; 
-done
+for i in "${volunteers[@]}" ; do echo "insert into volunteers (name, email, slack_user_id, pronouns, project_id, created_at, updated_at) values ('$i', 'fake.volunteer.$(((RANDOM%99999)+1))@test.null', 'U03AU1WDP5Z', 'lorum/ipsum', '$(((RANDOM%4)+1))', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);" >> seed.psql; done
 
 # Add test Volunteers assigned to no projects.
-for i in "${unassigned_volunteers[@]}"
-do
-    echo "insert into volunteers (name, email, slack_user_id, pronouns, created_at, updated_at) values ('$i', 'fake.volunteer.$(((RANDOM%99999)+1))@test.null', 'U03AU1WDP5Z', 'lorum/ipsum', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);" >> seed.psql; 
-done
+for i in "${unassigned_volunteers[@]}" ; do echo "insert into volunteers (name, email, slack_user_id, pronouns, created_at, updated_at) values ('$i', 'fake.volunteer.$(((RANDOM%99999)+1))@test.null', 'U03AU1WDP5Z', 'lorum/ipsum', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);" >> seed.psql; done
 
-for ((n=1; n<=15; n++));
-do
-    echo "insert into \"VolunteerSkills\" (volunteer_id, skill_id, created_at, updated_at) values ('$n', '$(((RANDOM%6)+1))', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);" >> seed.psql;
-done
+# Add test Volunteer Skills.
+for ((n=1; n<=15; n++)) ; do echo "insert into \"VolunteerSkills\" (volunteer_id, skill_id, created_at, updated_at) values ('$n', '$(((RANDOM%6)+1))', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);" >> seed.psql; done
 
 lando ssh -s postgresService -c "psql -U postgres -d voma -f ./seed.psql";
 rm seed.psql;
