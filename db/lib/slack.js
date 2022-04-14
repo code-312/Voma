a5639b79617caca5d4052606859c4e1c955929b2
Adam <innesian@github.com>
2022-04-14 07:45:41 -0500

    Finishing message channel and message user function. Tested with a new attached bot on the api-test workspace. Functions require [chat:write], [channels:read] permissions enabled on the bot.

--- db/lib/slack.js
+++ db/lib/slack.js
@@ -1,22 +1,56 @@
 require('dotenv').config({ path: `../.env.${process.env.NODE_ENV}`});
 
 const axios = require('axios');
-const apiEndpoint = 'https://slack.com/api/chat.postMessage';
+const apiEndpoint = 'https://slack.com/api';
 
 /**
- * Function for sending a message to a channel.
+ * Get ID of channel if it exists..
  * 
- * Requires [chat:write] permission from the Slack API.
+ * Requires [channels:read] permissions from the Slack API.
  * 
- * @param {string} channel - Channel to post to, for example #voma.
+ * @param {string} channel - Channel name (no hashtag) for example "voma".
+ */
+ const slackGetChannelId = async (channel) => {
+    try {
+        const response = await axios.get(`${apiEndpoint}/conversations.list`,{
+            headers: {
+                'Authorization': `Bearer ${process.env.SLACK_BOT_TOKEN}`
+            }
+        });
+
+        let channelId = '';
+        if (response.data?.ok && response.data?.channels) {
+            for (let i=0; i<response.data.channels.length; i++) {
+                if (channel == response.data.channels[i]?.name) {
+                    channelId = response.data.channels[i]?.id;
+                }
+            }
+        }
+
+        return channelId;
+
+    } catch (e) {
+        console.error(e);
+        return false;
+    }
+}
+
+/**
+ * Send a slack message to a channel.
+ * 
+ * Requires [chat:write] and [channels:read] permissions from the Slack API.
+ * 
+ * @param {string} channel - Channel to post to (no hashtag), for example "voma".
  * @param {string} message - Message to post to the channel.
  */
  const slackMessageChannel = async (channel, message) => {
     if (!channel || !message) return false;
 
+    const channelId = await slackGetChannelId(channel);
+
     try {
         const params = {
-            channel,
+            channel: channelId,
             text: message,
         };
         const options = {
@@ -24,7 +58,7 @@ const apiEndpoint = 'https://slack.com/api/chat.postMessage';
                 Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`
             }
         };
-        const result = await axios.post(`${apiEndpoint}/api/chat.postMessage`, params, options);
+        const result = await axios.post(`${apiEndpoint}/chat.postMessage`, params, options);
 
         return result;
 
@@ -34,8 +68,9 @@ const apiEndpoint = 'https://slack.com/api/chat.postMessage';
     }
 }
 
+
 /**
- * Function for sending a message to a user.
+ * Send a slack message to a user.
  * 
  * Requires [chat:write] permission from the Slack API.
  * 
@@ -55,7 +90,7 @@ const apiEndpoint = 'https://slack.com/api/chat.postMessage';
                 Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`
             }
         };
-        const result = await axios.post(`${apiEndpoint}/api/chat.postMessage`, params, options);
+        const result = await axios.post(`${apiEndpoint}/chat.postMessage`, params, options);
 
         return result;
 
@@ -68,7 +103,7 @@ const apiEndpoint = 'https://slack.com/api/chat.postMessage';
 /**
  * Lookup a user by email.
  * 
- * Requires [users:read.email] permission from the Slack API.
+ * Requires [users.read] and [users:read.email] permissions from the Slack API.
  * 
  * @param {string} email - Email to get profile for.
  */
@@ -76,7 +111,7 @@ const apiEndpoint = 'https://slack.com/api/chat.postMessage';
     if (!email) return false;
 
     try {
-        const result = await axios.get('https://slack.com/api/users.lookupByEmail', {
+        const result = await axios.get(`${apiEndpoint}/users.lookupByEmail`, {
             params: { email },
             headers: {
                 'Authorization': `Bearer ${process.env.SLACK_BOT_TOKEN}`
@@ -92,6 +127,7 @@ const apiEndpoint = 'https://slack.com/api/chat.postMessage';
 }
 
 module.exports = {
+    slackGetChannelId,
     slackMessageChannel,
     slackMessageUser,
     slackLookupByEmail,
