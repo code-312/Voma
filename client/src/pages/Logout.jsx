25ee01e9f8ba123ca30d1ba03f5b955b0e87a29b
Adam <code@adaminnes.com>
2022-04-16 17:00:31 -0500

    Adding logout route.

---
+++ client/src/pages/Logout.jsx
@@ -0,0 +1,11 @@
+import { useContext } from 'react';
+import { AuthContext } from '../lib/AuthProvider';
+
+export default function Logout() {
+    const AuthUser = useContext(AuthContext);
+    AuthUser.logout();
+
+    return (<>
+    
+    </>);
+}
