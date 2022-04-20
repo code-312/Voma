e80dbb7b6f223c7f7a5caf873c1f3112d2efbbfe
Adam <code@adaminnes.com>
2022-04-20 17:17:09 -0500

    Route should use GET.

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/api/admin/:id', verifyAuth, adminController.getAdmin);
 /*========= AUTHENTICATION ROUTES =========*/
 app.post('/api/login', adminController.login);
 app.get('/api/logout', adminController.logout);
-app.post('/api/authenticated', adminController.loginState);
+app.get('/api/authenticated', adminController.loginState);
 
 // 404 error
 app.use(function(req, res, next) {
