099392dd9cf30062b0d6163df1564cda292e0a70
raindogg <wabray17@gmail.com>
2022-05-31 20:28:01 -0500

    Create migration and model for Link table

--- db/models/addAssociations.js
+++ db/models/addAssociations.js
@@ -1,13 +1,16 @@
 const { sequelize } = require('sequelize');
 
 function addAssociations(sequelize) {
-  const {skill, volunteer, project} = sequelize.models;
+  const {skill, volunteer, project, link} = sequelize.models;
 
   volunteer.belongsToMany(skill, {through: "VolunteerSkills"});
   skill.belongsToMany(volunteer, {through: "VolunteerSkills"});
 
   project.hasMany(volunteer);
   volunteer.belongsTo(project);
+
+  project.hasMan(link);
+  link.belongsTo(project);
 }
 
 module.exports = { addAssociations };
