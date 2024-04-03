909bc206b2581bb72b04b3efba4fbd294e061df0
Doug Leinen <doug@geletkaplus.com>
2024-04-03 06:51:02 -0500

    add styled thankyou

--- client/src/lib/VolunteerProvider.js
+++ client/src/lib/VolunteerProvider.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext, useEffect } from "react";
+import React, { useState, createContext, useEffect } from 'react';
 
 const VolunteerContext = createContext(null);
 
@@ -6,39 +6,40 @@ function VolunteerProvider({ children }) {
   // Use localstorage for the moment until we get Sessions figured out.
   const storedProfile = localStorage.getItem('volunteer');
   const storedRegistrationStep = localStorage.getItem('registrationStep');
-  
+
   let defaultProfile = {
-      isAuthenticated: false,
-      notRegistered: false,
-      email: '',
-      skills: '',
-      pronouns: '',
+    isAuthenticated: false,
+    notRegistered: false,
+    email: '',
+    skills: '',
+    pronouns: '',
   };
   let defaultRegistrationStep = -1;
 
-  if (storedProfile) { // If profile is in localstorage, use that.
+  if (storedProfile) {
+    // If profile is in localstorage, use that.
     defaultProfile = JSON.parse(storedProfile);
   }
 
   if (storedRegistrationStep) {
     defaultRegistrationStep = parseInt(storedRegistrationStep, 10); // Force integer type.
   }
-  
+
   const [profile, setProfile] = useState(defaultProfile);
   const [registrationStep, setRegistrationStep] = useState(defaultRegistrationStep);
   const [registrationErrorMessage, setRegistrationErrorMessage] = useState('');
 
   function updateInfo(info) {
-    let p = {}; 
+    let p = {};
     p = Object.assign(profile, info);
     setProfile(p);
     localStorage.setItem('volunteer', JSON.stringify(p));
-  };
+  }
 
   /**
    * Check if this email is signed up for the CFC Slack workspace.
-   * 
-   * @param {string} volunteerEmail 
+   *
+   * @param {string} volunteerEmail
    */
   const slackExists = (email) => {
     fetch(`/api/volunteer/slack`, {
@@ -46,57 +47,56 @@ function VolunteerProvider({ children }) {
       body: JSON.stringify({ email }),
       headers: {
         'Content-Type': 'application/json',
-      }
+      },
     })
-    .then((data) => {
-      if (data.status === 404) {
-        console.log(data);
-        throw new Error('404: Route not found.');
-      } else return data.json();
-    })
-    .then((response) => {
-      let profileUpdate = {};
+      .then((data) => {
+        if (data.status === 404) {
+          console.log(data);
+          throw new Error('404: Route not found.');
+        } else return data.json();
+      })
+      .then((response) => {
+        let profileUpdate = {};
 
-      if (response.exists) {  // User found.
-        profileUpdate = Object.assign(profile, {
+        if (response.exists) {
+          // User found.
+          profileUpdate = Object.assign(profile, {
+            isAuthenticated: true,
+            email,
+            notRegistered: false,
+            suid: response.suid,
+            name: response.name,
+          });
+          updateInfo(profileUpdate);
+          setRegistrationStep(1);
+        } else {
+          profileUpdate = Object.assign(profile, {
+            isAuthenticated: false,
+            notRegistered: true,
+            email: '',
+            skills: '',
+            pronouns: '',
+          });
+          updateInfo(profileUpdate);
+          setRegistrationStep(1);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+
+        // For now, fake successful return of profile.
+        console.log('Faking successful signin for now, for development.');
+        const profileUpdate = Object.assign(profile, {
           isAuthenticated: true,
           email,
-          notRegistered: false, 
-          suid: response.suid,
-          name: response.name,
+          notRegistered: false,
+          suid: 'FAKE_API_USER',
+          name: 'Fake User',
         });
         updateInfo(profileUpdate);
         setRegistrationStep(1);
-
-      } else {
-        profileUpdate = Object.assign(profile, {
-          isAuthenticated: false,
-          notRegistered: true,
-          email: '',
-          skills: '',
-          pronouns: '',
-        });
-        updateInfo(profileUpdate); 
-        setRegistrationStep(1)
-      }
-
-    })
-    .catch((err) => {
-      console.log(err);
-
-      // For now, fake successful return of profile.
-      console.log('Faking successful signin for now, for development.');
-      const profileUpdate = Object.assign(profile, {
-        isAuthenticated: true,
-        email,
-        notRegistered: false,
-        suid: 'FAKE_API_USER',
-        name: 'Fake User',
+        // \fake successful return of profile.
       });
-      updateInfo(profileUpdate);
-      setRegistrationStep(1);
-      // \fake successful return of profile.
-    });
   };
 
   const registerVolunteer = () => {
@@ -111,30 +111,36 @@ function VolunteerProvider({ children }) {
       }),
       headers: {
         'Content-Type': 'application/json',
-      }
+      },
     })
-    .then((data) => {
-      if (data.status === 404) {        
-        setRegistrationErrorMessage('Oops, something went wrong. Please reach out on Slack for help registering.');
+      .then((data) => {
+        if (data.status === 404) {
+          setRegistrationErrorMessage(
+            'Oops, something went wrong. Please reach out on Slack for help registering.',
+          );
+          window.scrollY = 0;
+
+          throw new Error('404: Route not found.');
+        } else return data.json();
+      })
+      .then((response) => {
+        if (response.success) {
+          setRegistrationStep(4);
+        } else {
+          console.log(response);
+          setRegistrationErrorMessage(
+            'Oops, something went wrong. Please reach out on Slack for help registering.',
+          );
+          window.scrollY = 0;
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        setRegistrationErrorMessage(
+          'Oops, something went wrong. Please reach out on Slack for help registering.',
+        );
         window.scrollY = 0;
-        
-        throw new Error('404: Route not found.');
-      } else return data.json();
-    })
-    .then(response => {
-      if (response.success) {
-        setRegistrationStep(4);
-      } else {
-        console.log(response);
-        setRegistrationErrorMessage('Oops, something went wrong. Please reach out on Slack for help registering.');
-        window.scrollY = 0;
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      setRegistrationErrorMessage('Oops, something went wrong. Please reach out on Slack for help registering.');
-      window.scrollY = 0;
-    });
+      });
   };
 
   useEffect(() => {
@@ -144,21 +150,22 @@ function VolunteerProvider({ children }) {
   useEffect(() => {
     localStorage.setItem('volunteer', JSON.stringify(profile));
   }, [profile]);
-  
-  const funcs = { 
+
+  const funcs = {
     setProfile,
-    slackExists, 
+    slackExists,
     setRegistrationStep,
     updateInfo,
     registerVolunteer,
   };
 
   return (
-    <VolunteerContext.Provider value={{ ...profile, registrationStep, registrationErrorMessage, ...funcs}}>
-        {children}
+    <VolunteerContext.Provider
+      value={{ ...profile, registrationStep, registrationErrorMessage, ...funcs }}
+    >
+      {children}
     </VolunteerContext.Provider>
   );
 }
 
 export { VolunteerProvider, VolunteerContext };
-
