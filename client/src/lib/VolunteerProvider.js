a5f2013c5b467f491e95b37be46f450752041653
Doug Leinen <doug@geletkaplus.com>
2024-04-03 06:51:48 -0500

    revert register state

--- client/src/lib/VolunteerProvider.js
+++ client/src/lib/VolunteerProvider.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext, useEffect } from 'react';
+import React, { useState, createContext, useEffect } from "react";
 
 const VolunteerContext = createContext(null);
 
@@ -8,16 +8,15 @@ function VolunteerProvider({ children }) {
   const storedRegistrationStep = localStorage.getItem('registrationStep');
 
   let defaultProfile = {
-    isAuthenticated: false,
-    notRegistered: false,
-    email: '',
-    skills: '',
-    pronouns: '',
+      isAuthenticated: false,
+      notRegistered: false,
+      email: '',
+      skills: '',
+      pronouns: '',
   };
   let defaultRegistrationStep = -1;
 
-  if (storedProfile) {
-    // If profile is in localstorage, use that.
+  if (storedProfile) { // If profile is in localstorage, use that.
     defaultProfile = JSON.parse(storedProfile);
   }
 
@@ -34,7 +33,7 @@ function VolunteerProvider({ children }) {
     p = Object.assign(profile, info);
     setProfile(p);
     localStorage.setItem('volunteer', JSON.stringify(p));
-  }
+  };
 
   /**
    * Check if this email is signed up for the CFC Slack workspace.
@@ -47,56 +46,57 @@ function VolunteerProvider({ children }) {
       body: JSON.stringify({ email }),
       headers: {
         'Content-Type': 'application/json',
-      },
+      }
     })
-      .then((data) => {
-        if (data.status === 404) {
-          console.log(data);
-          throw new Error('404: Route not found.');
-        } else return data.json();
-      })
-      .then((response) => {
-        let profileUpdate = {};
+    .then((data) => {
+      if (data.status === 404) {
+        console.log(data);
+        throw new Error('404: Route not found.');
+      } else return data.json();
+    })
+    .then((response) => {
+      let profileUpdate = {};
 
-        if (response.exists) {
-          // User found.
-          profileUpdate = Object.assign(profile, {
-            isAuthenticated: true,
-            email,
-            notRegistered: false,
-            suid: response.suid,
-            name: response.name,
-          });
-          updateInfo(profileUpdate);
-          setRegistrationStep(1);
-        } else {
-          profileUpdate = Object.assign(profile, {
-            isAuthenticated: false,
-            notRegistered: true,
-            email: '',
-            skills: '',
-            pronouns: '',
-          });
-          updateInfo(profileUpdate);
-          setRegistrationStep(1);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-
-        // For now, fake successful return of profile.
-        console.log('Faking successful signin for now, for development.');
-        const profileUpdate = Object.assign(profile, {
+      if (response.exists) {  // User found.
+        profileUpdate = Object.assign(profile, {
           isAuthenticated: true,
           email,
           notRegistered: false,
-          suid: 'FAKE_API_USER',
-          name: 'Fake User',
+          suid: response.suid,
+          name: response.name,
         });
         updateInfo(profileUpdate);
         setRegistrationStep(1);
-        // \fake successful return of profile.
+
+      } else {
+        profileUpdate = Object.assign(profile, {
+          isAuthenticated: false,
+          notRegistered: true,
+          email: '',
+          skills: '',
+          pronouns: '',
+        });
+        updateInfo(profileUpdate);
+        setRegistrationStep(1)
+      }
+
+    })
+    .catch((err) => {
+      console.log(err);
+
+      // For now, fake successful return of profile.
+      console.log('Faking successful signin for now, for development.');
+      const profileUpdate = Object.assign(profile, {
+        isAuthenticated: true,
+        email,
+        notRegistered: false,
+        suid: 'FAKE_API_USER',
+        name: 'Fake User',
       });
+      updateInfo(profileUpdate);
+      setRegistrationStep(1);
+      // \fake successful return of profile.
+    });
   };
 
   const registerVolunteer = () => {
@@ -111,36 +111,30 @@ function VolunteerProvider({ children }) {
       }),
       headers: {
         'Content-Type': 'application/json',
-      },
+      }
     })
-      .then((data) => {
-        if (data.status === 404) {
-          setRegistrationErrorMessage(
-            'Oops, something went wrong. Please reach out on Slack for help registering.',
-          );
-          window.scrollY = 0;
-
-          throw new Error('404: Route not found.');
-        } else return data.json();
-      })
-      .then((response) => {
-        if (response.success) {
-          setRegistrationStep(4);
-        } else {
-          console.log(response);
-          setRegistrationErrorMessage(
-            'Oops, something went wrong. Please reach out on Slack for help registering.',
-          );
-          window.scrollY = 0;
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        setRegistrationErrorMessage(
-          'Oops, something went wrong. Please reach out on Slack for help registering.',
-        );
+    .then((data) => {
+      if (data.status === 404) {
+        setRegistrationErrorMessage('Oops, something went wrong. Please reach out on Slack for help registering.');
         window.scrollY = 0;
-      });
+
+        throw new Error('404: Route not found.');
+      } else return data.json();
+    })
+    .then(response => {
+      if (response.success) {
+        setRegistrationStep(4);
+      } else {
+        console.log(response);
+        setRegistrationErrorMessage('Oops, something went wrong. Please reach out on Slack for help registering.');
+        window.scrollY = 0;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      setRegistrationErrorMessage('Oops, something went wrong. Please reach out on Slack for help registering.');
+      window.scrollY = 0;
+    });
   };
 
   useEffect(() => {
@@ -160,12 +154,11 @@ function VolunteerProvider({ children }) {
   };
 
   return (
-    <VolunteerContext.Provider
-      value={{ ...profile, registrationStep, registrationErrorMessage, ...funcs }}
-    >
-      {children}
+    <VolunteerContext.Provider value={{ ...profile, registrationStep, registrationErrorMessage, ...funcs}}>
+        {children}
     </VolunteerContext.Provider>
   );
 }
 
 export { VolunteerProvider, VolunteerContext };
+
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -10,20 +10,19 @@ import ApiError from '../components/ApiError';
 import { VolunteerContext } from '../lib/VolunteerProvider';
 import useTitle from '../hooks/useTitle';
 
+
 export default function Register() {
-  useTitle('Voma | Register');
+  useTitle('Voma | Register')
   const Volunteer = useContext(VolunteerContext);
 
-  return (
-    <>
-      {/* {!Volunteer.isAuthenticated && <Redirect to="/" />} */}
-      {/* {Volunteer.registrationErrorMessage && <ApiError message={Volunteer.registrationErrorMessage} />} */}
-      <form>
-        {/* {Volunteer?.registrationStep === 1 && <BasicInfoForm />}
+  return (<>
+    {!Volunteer.isAuthenticated && <Redirect to="/" />}
+    {Volunteer.registrationErrorMessage && <ApiError message={Volunteer.registrationErrorMessage} />}
+    <form>
+      {Volunteer?.registrationStep === 1 && <BasicInfoForm />}
       {Volunteer?.registrationStep === 2 && <SkillsForm />}
-      {Volunteer?.registrationStep === 3 && <CodeOfConduct />} */}
-        {Volunteer?.registrationStep === 4 && <ThankYouMessage />}
-      </form>
-    </>
-  );
+      {Volunteer?.registrationStep === 3 && <CodeOfConduct />}
+      {Volunteer?.registrationStep === 4 && <ThankYouMessage />}
+    </form>
+  </>);
 }
