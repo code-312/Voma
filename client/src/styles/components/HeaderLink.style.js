60007e02403df775b9e0fca92efe90c3d5543825
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2024-06-01 13:18:29 -0500

    Add flex to volunteer modal link header section, fix border off center

--- client/src/styles/components/HeaderLink.style.js
+++ client/src/styles/components/HeaderLink.style.js
@@ -1,23 +1,25 @@
 import styled from 'styled-components';
 
 export const HeaderLinksWrapper = styled.div`
-    display: flex;
+  display: flex;
 `;
 
 export const HeaderLinkContainer = styled.div`
-    ${({ active }) => active && 'border-bottom: solid 3px var(--uiBlue);'}
+  ${({ active }) => active && 'border-bottom: solid 3px var(--uiBlue);'}
+  display: flex;
 `;
 
 export const HeaderLink = styled.button`
-    border: none;
-    font-size: ${({ variant }) => variant === 'large' ? '18' : '14'}px;
-    color: var(--managementBlue);
-    width: 100%;
-    margin: 16px 8px;
-    padding: 16px 12px;
-    ${({ forHeader }) => forHeader && `
+  border: none;
+  font-size: ${({ variant }) => (variant === 'large' ? '18' : '14')}px;
+  color: var(--managementBlue);
+  width: 100%;
+  margin: 16px 8px;
+  padding: 16px 12px;
+  ${({ forHeader }) =>
+    forHeader &&
+    `
         height: 56px;
         margin: 0;
     `}
-
 `;
