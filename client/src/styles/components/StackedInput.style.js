fb92468d8e5fc388ed56c4f2744c0a9f0bce8832
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2024-06-15 13:32:54 -0500

    Edit form input hover states

--- client/src/styles/components/StackedInput.style.js
+++ client/src/styles/components/StackedInput.style.js
@@ -1,24 +1,32 @@
 import styled from 'styled-components';
 
 export const StackedInputContainer = styled.div`
-    padding: 16px 12px;
-    border-radius: 3px;
-    border: 0.5px solid var(--blueShade2);
-    background: var(--white);
-    margin: 2px 0;
-    ${({ checked }) =>
+  padding: 16px 12px;
+  border-radius: 3px;
+  border: 0.5px solid var(--blueShade2);
+  background: var(--white);
+  margin: 2px 0;
+
+  &:hover:not(:checked) {
+    background: rgba(117, 123, 142, 0.2);
+    cursor: pointer;
+
+    & label {
+      cursor: pointer;
+    }
+  }
+
+  ${({ checked }) =>
     checked &&
     `
     background-color: var(--uiBlue);
     color: var(--white);
   `}
-   input {
-        margin-right: 8px;
-    
-   }
-   label {
-        display: block;
-        width: 100%;
-   }
+  input {
+    margin-right: 8px;
+  }
+  label {
+    display: block;
+    width: 100%;
+  }
 `;
-
